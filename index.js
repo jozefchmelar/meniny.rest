3869ddb6d76a2f9f92001d145bfa792fbc0e4f11
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2018-07-09 13:49:22 +0200

    formating.

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 const express = require('express')
 const app = express()
-const router = express.Router()
-const path = require('path')
-const fs = require('fs');
+const fs = require('fs')
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, 'public')))
+fs.readFile('sk-meniny.csv', 'utf8', (err, data) => {
+  const toMonthDay = (month, day) => month + "" + day
+  var namedays = {}
+  data.split("\n").forEach(nameday => {
+    //03-19,Jozef -> "319" "Jozef"
+    const month = Number(nameday.split(",")[0].split("-")[0])
+    const day = Number(nameday.split(",")[0].split("-")[1])
+    const name = nameday.split(",")[1]
+    namedays[toMonthDay(month,day)] = name
+  })
 
-fs.readFile('sk-meniny.csv','utf8', (err,data) => {
-    var namedays = data.split("\n")
-    var nameDaysDictionary = {}    
-    namedays.forEach(nameday => {
-      const month = Number(nameday.split(",")[0].split("-")[0])
-      const day   = Number(nameday.split(",")[0].split("-")[1])
-      const monthDay = month + "" + day
-      const name  = nameday.split(",")[1]
-      nameDaysDictionary[monthDay] = name
-    });        
+  app.get('/', (req, res) => {
+    const today = new Date()
+    const month = Number(today.getMonth() + 1)
+    const day = Number(today.getDate())
+    res.json({ "nameday": namedays[toMonthDay(month,day)] })
+  })
 
-    app.get('/', (req,res)=>{
-      const today = new Date();
-      const month = Number(today.getMonth()+1)
-      const day = Number(today.getDate())
-      res.json({ "nameday" : nameDaysDictionary[month+ "" +day]})
-    })
+  app.get('/month/:mesiac/day/:den', (req, res) => {
+    const month = Number(req.params.mesiac)
+    const day = Number(req.params.den)
+    res.json({ "nameday": namedays[toMonthDay(month,day)] })
+  })
 
-    app.get('/month/:mesiac/day/:den', (req,res) => {
-      const month = Number(req.params.mesiac)
-      const day   = Number(req.params.den)
-      res.json({ "nameday" : nameDaysDictionary[month+ "" +day]})
-    })
-
-    app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-  }) ;
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+})
